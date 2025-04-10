e989d263d8e560845636198a730e491ed513c613
eiiyan <yanpandim77@gmail.com>
2025-04-10 13:41:08 -0300

    Implementado Model , Migrate, Index e Seeder do CRUD de funcionarios

---
+++ database/seeders/FuncionarioSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Funcionario;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FuncionarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Funcionario::create([
+            'nome' => 'Yan Gabriel',
+            'cpf' => '12345678911',
+            'email' => 'funcionario1@exemplo.com',
+            'senha'=> bcrypt('senha123')
+        ]);
+
+        Funcionario::create([
+            'nome' => 'Yan Gabriel',
+            'cpf' => '12345678912',
+            'email' => 'funcionario2@exemplo.com',
+            'senha'=> bcrypt('senha123')
+        ]);
+
+        Funcionario::create([
+            'nome' => 'Yan Gabriel',
+            'cpf' => '12345678913',
+            'email' => 'funcionario3@exemplo.com',
+            'senha'=> bcrypt('senha123')
+        ]);
+
+        // Funcionario::factory()->count(15)->create();
+    }
+}
